c495a9e779ca66d30f471700cf29db4f82482e2b
Philz <dlim@pm-studios.com>
2024-09-02 15:33:11 +0900

    deploy

---
+++ precache-manifest.3b79e582cb913dcc1150faf92db932bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef13ad9fbc1cd151b1da23461d623266",
+    "url": "/redeem_wsr/index.html"
+  },
+  {
+    "revision": "54ba0d771ef97495b6d0",
+    "url": "/redeem_wsr/static/css/main.953250c6.chunk.css"
+  },
+  {
+    "revision": "1f1fcca4972c7c3103c8",
+    "url": "/redeem_wsr/static/js/2.5070ebec.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/redeem_wsr/static/js/2.5070ebec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54ba0d771ef97495b6d0",
+    "url": "/redeem_wsr/static/js/main.0b6142ad.chunk.js"
+  },
+  {
+    "revision": "75c31b3cf0564f8e9cc9",
+    "url": "/redeem_wsr/static/js/runtime-main.aad47e7f.js"
+  }
+]);
